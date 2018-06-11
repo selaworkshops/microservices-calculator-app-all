0eeb173991e091e1ed57127cd29e527147eb31bb
Leon Jalfon <leonj@sela.co.il>
2018-06-11 20:28:07 +0300

    fix response + refactoring

--- routes/routes.js
+++ routes/routes.js
@@ -1,16 +1,24 @@
 var appRouter = function (app) {
-var division = require('./../division.js');
-var os = require('os');
 
 app.get("/", function(req, res) {
 res.status(404).send("Error 404, To use the division service browse to: {server}:{port}/division/{number1}/{number2}");
 })
   
 app.get("/division/:number1/:number2", function(req,res) {
-    var number1 = req.params.number1;
-    var number2 = req.params.number2;
-    var total = division.division(number1,number2);
-    res.send(JSON.stringify(total));
+    var number1 = parseInt(req.params.number1);
+    var number2 = parseInt(req.params.number2);
+    var total = number1 / number2;
+
+	var body = {
+		"first-number": number1,
+		"second-number": number2,
+		"operation": "division",
+		"total": total
+	}
+	
+	res.setHeader('Access-Control-Allow-Origin', 'null');
+	return res.status(200).end(JSON.stringify(body));
+	
   });
 }
 
